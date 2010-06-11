93cf59bb7d665ed43dfaf02c946f91dc5c3b1a05
Julien Lesgourgues <lesgourg@mail.cern.ch>
2010-06-11 12:52:08 +0000

    adding class.h

---
+++ include/class.h
@@ -0,0 +1,29 @@
+#ifndef __CLASS__
+#define __CLASS__
+
+/* standard libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <float.h>
+
+/* tools for class */
+#include "growTable.h"
+#include "tools_arrays.h"
+#include "dei_rkck.h"
+#include "parser.h"
+
+/* class modules */
+#include "common.h"
+#include "precision.h"
+#include "background.h"
+#include "thermodynamics.h"
+#include "perturbations.h"
+#include "bessel.h"
+#include "transfer.h"
+#include "primordial.h"
+#include "spectra.h"
+#include "output.h"
+
+#endif
