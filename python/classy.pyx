81ae2be4f194f65b2e551611ab5e221ede485867
Benjamin Audren <benj_audren@yahoo.fr>
2011-11-20 17:19:50 +0000

    classy.pyx finalized, now work with lensed_cl again

--- python/classy.pyx
+++ python/classy.pyx
@@ -457,7 +457,7 @@ cdef class Class:
       for md from 0<=md<4:
         cl[md,ell] = lcl[md]
       
-    self._struct_cleanup(self.ncp)
+    #self._struct_cleanup(self.ncp)
     return cl
     
   def pk_l (self,double z=0,k=None,nofail=False):
