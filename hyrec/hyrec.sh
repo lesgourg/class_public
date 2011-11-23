f737ba70b0fdd9a589e9d598ce05e5d4aa08ae09
Julien Lesgourgues <lesgourg@mail.cern.ch>
2011-11-23 23:45:42 +0000

    add only useful files in hyrec directory

--- hyrec/hyrec.sh
+++
@@ -1,8 +0,0 @@
-rm *.o
-gcc -03 -c arrays.c -o arrays.o
-gcc -03 -c helium.c -o helium.o
-gcc -03 -c hydrogen.c -o hydrogen.o
-gcc -03 -c history.c -o history.o
-gcc -03 -c hyrec.c -o hyrec.o
-gcc -03 arrays.o helium.o hydrogen.o history.o hyrec.o -o hyrec
-ar rv libhyrec.a arrays.o helium.o hydrogen.o history.o
