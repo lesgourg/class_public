bf47075674313724b088d7e766ea34f8910fe0a7
Julien Lesgourgues <lesgourg@mail.cern.ch>
2012-10-23 19:01:01 +0000

    correct error in m_ncdm_tot formula of classy

--- python/classy.pyx
+++ python/classy.pyx
@@ -741,7 +741,7 @@ cdef class Class:
       if elem == 'm_ncdm_in_eV':
         data.mcmc_parameters[elem]['current'] = self.ba.m_ncdm_in_eV[0]
       if elem == 'm_ncdm_tot':
-        data.mcmc_parameters[elem]['current'] = self.ba.Omega0_ncdm_tot*self.ba.h*self.ba.h/93.14
+        data.mcmc_parameters[elem]['current'] = self.ba.Omega0_ncdm_tot*self.ba.h*self.ba.h*93.14
       if elem == 'Neff':
         data.mcmc_parameters[elem]['current'] = self.ba.Neff
       if elem == 'tau_reio':
