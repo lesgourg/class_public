824dcdd4348d7fbb0c1b1b3f5be2d0f2e4217129
Julien Lesgourgues <lesgourg@cern.ch>
2014-10-02 16:23:42 +0200

    default values and explanatory values set to Planck2013+WP bestfit

--- source/input.c
+++ source/input.c
@@ -2559,13 +2559,13 @@ int input_default_params(
 
   /** - background structure */
 
-  pba->h = 0.704;
+  pba->h = 0.6704;
   pba->H0 = pba->h * 1.e5 / _c_;
-  pba->T_cmb = 2.726;
+  pba->T_cmb = 2.7255;
   pba->Omega0_g = (4.*sigma_B/_c_*pow(pba->T_cmb,4.)) / (3.*_c_*_c_*1.e10*pba->h*pba->h/_Mpc_over_m_/_Mpc_over_m_/8./_PI_/_G_);
   pba->Omega0_ur = 3.046*7./8.*pow(4./11.,4./3.)*pba->Omega0_g;
-  pba->Omega0_b = 0.02253/0.704/0.704;
-  pba->Omega0_cdm = 0.1122/0.704/0.704;
+  pba->Omega0_b = 0.022032/pow(pba->h,2);
+  pba->Omega0_cdm = 0.12038/pow(pba->h,2);
   pba->Omega0_dcdmdr = 0.0;
   pba->Omega0_dcdm = 0.0;
   pba->Gamma_dcdm = 0.0;
@@ -2605,10 +2605,10 @@ int input_default_params(
   pth->recombination=recfast;
   pth->reio_parametrization=reio_camb;
   pth->reio_z_or_tau=reio_z;
-  pth->z_reio=10.3;
-  pth->tau_reio=0.085;
+  pth->z_reio=11.357;
+  pth->tau_reio=0.0925;
   pth->reionization_exponent=1.5;
-  pth->reionization_width=1.5;
+  pth->reionization_width=0.5;
   pth->helium_fullreio_redshift=3.5;
   pth->helium_fullreio_width=0.5;
 
@@ -2692,8 +2692,8 @@ int input_default_params(
 
   ppm->primordial_spec_type = analytic_Pk;
   ppm->k_pivot = 0.002;
-  ppm->A_s = 2.42e-9;
-  ppm->n_s = 0.967;
+  ppm->A_s = 2.215e-9;
+  ppm->n_s = 0.9619;
   ppm->alpha_s = 0.;
   ppm->f_bi = 1.;
   ppm->n_bi = 1.;
