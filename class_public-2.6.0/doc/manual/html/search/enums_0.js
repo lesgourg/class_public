eaed73379d95734f73a31a77aeaf68bcb604442f
lesgourg <lesgourg@physik.rwth-aachen.de>
2017-07-02 18:31:39 +0200

    removed v2.6.0

--- class_public-2.6.0/doc/manual/html/search/enums_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['evolver_5ftype',['evolver_type',['../common_8h.html#a554a0cdfc80aa49273197d324b2e9956',1,'common.h']]]
-];
--- class_public-2.6.0/doc/manual/html/search/files_6.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['perturbations_2ec',['perturbations.c',['../perturbations_8c.html',1,'']]],
-  ['perturbations_2eh',['perturbations.h',['../perturbations_8h.html',1,'']]],
-  ['primordial_2ec',['primordial.c',['../primordial_8c.html',1,'']]],
-  ['primordial_2eh',['primordial.h',['../primordial_8h.html',1,'']]]
-];
