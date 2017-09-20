d49d0da9431c1b3af907ae9c6af0a2b7658d80e6
Patrick Stöcker <patrick.stoecker@rwth-aachen.de>
2017-09-20 16:38:39 +0200

    Updating the HiggsPortal-model

--- external_fz/models/HiggsPortal/prepare.py
+++ external_fz/models/HiggsPortal/prepare.py
@@ -1,58 +1,40 @@
-import itertools
 import dill
 import numpy as np
 import sys
-import os 
+import os
 if os.environ['DARKAGES_BASE']:
 	sys.path.insert(0, os.environ['DARKAGES_BASE'] )
 
 import DarkAges
-from DarkAges.common import channel_dict, get_index
-from DarkAges import logEnergies, redshift, transfer_functions, options
+from DarkAges.common import channel_dict
+from DarkAges import logEnergies
 from DarkAges.recipes import load_from_spectrum
-from DarkAges.model import model
 from DarkAges.interpolator import logInterpolator, logLinearInterpolator
 
 model_dir = os.path.split(os.path.realpath(__file__))[0]
 model_name =  model_dir.split('/')[-1]
 
-masses = np.genfromtxt(os.path.join(model_dir, 'data/Masses.dat'), unpack=True, usecols=(0), dtype=np.float64)
-masses = masses[masses <= 5e3]
-frac_read = np.genfromtxt(os.path.join(model_dir,'data/Fractions.dat'), unpack=True, usecols=(0,4,5,6,7,8,9,10,11,12), dtype=np.float64)
-primaries = np.array(['top', 'higgs', 'zzstar', 'wwstar', 'bottom', 'tau', 'charm', 'gluon', 'gamma'], dtype=np.dtype('a32'))
-#primaries = np.array(['top', 'higgs'], dtype=np.dtype('a32'))
+def prepare():
+	dump_dict = dict()
 
-fractions_interp_obj = np.empty(shape=(len(primaries)), dtype=logLinearInterpolator)
-for idx in xrange(len(primaries)):
-	fractions_interp_obj[idx] = logLinearInterpolator(frac_read[0,:], frac_read[idx+1,:], 0)
+	frac_read = np.genfromtxt(os.path.join(model_dir,'data/Fractions.dat'), unpack=True, usecols=(0,4,5,6,7,8,9,10,11,12), dtype=np.float64)
+	primaries = np.array(['top', 'higgs', 'zzstar', 'wwstar', 'bottom', 'tau', 'charm', 'gluon', 'gamma'], dtype=np.dtype('a32'))
+	#primaries = np.array(['top', 'higgs'], dtype=np.dtype('a32'))
 
-total = len(masses) * len(primaries)
-counter = 0. 
+	dump_dict.update({'channels':primaries})
 
-f_grid = np.empty(shape=(len(masses),len(primaries),5,len(redshift)), dtype = np.float64)
-for mass, primary in itertools.product(*(masses,primaries)):
-	counter += 1.
-	print (counter / total) * 100.
-	idx_mass = get_index(masses, mass)
-	idx_prim = get_index(primaries, primary)
-	fname = os.path.join(model_dir, 'data/{:s}_EW.dat'.format(primary))
-	tmp_model = load_from_spectrum(fname, logEnergies, mass, np.inf)
-	for channel in channel_dict:
-		idx_chan = channel_dict[channel]
-		f_grid[idx_mass,idx_prim,idx_chan,:] = tmp_model.calc_f(transfer_functions[idx_chan])[-1]
-		
-interpolated_f = np.empty(shape=(len(primaries),5,len(redshift)), dtype= logInterpolator)
-for primary, idx_chan, z in itertools.product(*(primaries,channel_dict.values(),redshift)):
-	idx_prim = get_index(primaries, primary)
-	idx_z = get_index(redshift, z)
-	interpolated_f[idx_prim, idx_chan, idx_z] = logInterpolator(masses, f_grid[:,idx_prim,idx_chan,idx_z], 0)	
+	for idx_prim, primary in enumerate(primaries):
+		dump_dict_key = 'frac_interp_{:s}'.format(primary)
+		dump_dict_value = logLinearInterpolator(frac_read[0,:], frac_read[idx_prim+1,:], 0,logspace_wanted=False)
+		dump_dict.update({dump_dict_key:dump_dict_value})
 
-dump_dict = {'fractions':fractions_interp_obj, 'f-functions': interpolated_f}
+		fname = os.path.join(model_dir, 'data/{:s}_EW.dat'.format(primary))
+		dump_dict_key = 'spec_interp_{:s}'.format(primary)
+		dump_dict_value = load_from_spectrum(fname, logEnergies)
+		dump_dict.update({dump_dict_key:dump_dict_value})
 
-with open(os.path.join(model_dir, '{}.obj'.format(model_name)),'wb') as dump_file:
-	dill.dump(dump_dict, dump_file)
-	
-		
-		
+	with open(os.path.join(model_dir, '{}.obj'.format(model_name)),'wb') as dump_file:
+		dill.dump(dump_dict, dump_file)
 
-
+if __name__ == "__main__":
+	prepare()
--- external_fz/models/HiggsPortal/run.py
+++ external_fz/models/HiggsPortal/run.py
@@ -1,47 +1,61 @@
-'''
-This model has two input parameters:
-    1) mass (between 50 and 100 GeV)
-    2) the relative contribution of muons as primaries	 
-'''
-
 import numpy as np
-import itertools
 import dill
-import os 
+import os
 import sys
 if os.environ['DARKAGES_BASE']:
 	sys.path.insert(0, os.environ['DARKAGES_BASE'] )
-import DarkAges
-from DarkAges.common import print_error, finalize, channel_dict, get_index
-from DarkAges import redshift, options
 
-if len(sys.argv) <2:
-	print_error("There are too few arguments passed. I expected at least 1")
-sampling_mass = float(sys.argv[1])
-if sampling_mass < 5 or sampling_mass > 5e3:
-	print_error("The mass-parameter sholud be in the range [5 GeV, 5 TeV]")
+import DarkAges
+from DarkAges.common import finalize, channel_dict
+from DarkAges.model import annihilating_model, decaying_model
+from DarkAges import redshift, DarkAgesError, transfer_functions
 
 model_dir = os.path.split(os.path.realpath(__file__))[0]
 model_name =  model_dir.split('/')[-1]
 
-with open(os.path.join(model_dir, '{}.obj'.format(model_name)),'rb') as dump_file:
-	dump_dict = dill.load(dump_file)
-	interpolated_f = dump_dict.get('f-functions')
-	interpolated_frac = dump_dict.get('fractions')
+def run( *arguments, **DarkOptions ):
+	if len(arguments) <2:
+		print_error("There are too few arguments passed. I expected at least 1")
+	sampling_mass = float(arguments[1])
+	if sampling_mass < 5 or sampling_mass > 5e3:
+		print_error("The mass-parameter sholud be in the range [5 GeV, 5 TeV]")
 
-f_functions = np.zeros(shape=(5,len(redshift)))
-for idx_chan, z in itertools.product(*(channel_dict.values(),redshift)):
-	idx_z = get_index(redshift, z)
-	temp_f = np.zeros(shape=(interpolated_f.shape[0]), dtype=np.float64)
-	temp_frac = np.zeros(shape=(interpolated_f.shape[0]), dtype=np.float64)
-	for idx_prim in xrange(interpolated_f.shape[0]):
-		temp_f[idx_prim] = interpolated_f[idx_prim,idx_chan,idx_z](sampling_mass)
-		temp_frac[idx_prim] = interpolated_frac[idx_prim](sampling_mass)
-	f_functions[idx_chan, idx_z] = np.tensordot(temp_f, temp_frac, axes=(0,0))
+	with open(os.path.join(model_dir, '{}.obj'.format(model_name)),'rb') as dump_file:
+		dump_dict = dill.load(dump_file)
 
-finalize(redshift, 
-         f_functions[channel_dict['Heat']], 
-         f_functions[channel_dict['Ly-A']], 
-         f_functions[channel_dict['H-Ion']],
-         f_functions[channel_dict['He-Ion']],
-         f_functions[channel_dict['LowE']])
+	primaries = dump_dict.get('channels')
+	tmp_shape = dump_dict.get('spec_interp_{:s}'.format(primaries[0]))._shape
+	total_spec = np.zeros(shape=tmp_shape, dtype=np.float64)
+	for primary in primaries:
+		fraction = dump_dict.get('frac_interp_{:s}'.format(primary)).__call__(sampling_mass)
+		spectra =  dump_dict.get('spec_interp_{:s}'.format(primary)).__call__(sampling_mass)
+		total_spec += fraction * spectra
+
+	history = DarkOptions.get('injection_history','annihilation')
+	if history == 'decay':
+		tdec = DarkOptions.get('t_dec')
+		full_model = decaying_model(total_spec[0], total_spec[1], total_spec[2], 1e9*sampling_mass, tdec)
+	else:
+		full_model = annihilating_model(total_spec[0], total_spec[1], total_spec[2], 1e9*sampling_mass)
+	#####
+
+	##### To finish the calculation, calculate f(z) for each deposition channel
+	## and print the CLASS-output with the finalize()-method
+	#####
+	f_functions = np.zeros((len(channel_dict),len(redshift)))
+	for channel in channel_dict:
+		idx = channel_dict[channel]
+		f_functions[idx,:] = full_model.calc_f(transfer_functions[idx])[-1]
+
+	finalize(redshift,
+             f_functions[channel_dict['Heat']],
+             f_functions[channel_dict['Ly-A']],
+             f_functions[channel_dict['H-Ion']],
+             f_functions[channel_dict['He-Ion']],
+             f_functions[channel_dict['LowE']],
+			 **DarkOptions)
+	#####
+
+if __name__ == "__main__":
+	from DarkAges import DarkOptions as milk
+	run( *sys.argv, **milk)
