8092db3b199aba79c9da9519f508eb75b6752939
lesgourg <lesgourg@physik.rwth-aachen.de>
2017-09-21 11:35:51 +0200

    fixed new precision settings for halofit

--- source/input.c
+++ source/input.c
@@ -2725,8 +2725,8 @@ int input_read_parameters(
 
   /** - (h.6.) parameters related to nonlinear calculations */
 
-  class_read_double("halofit_dz",ppr->halofit_dz);
   class_read_double("halofit_min_k_nonlinear",ppr->halofit_min_k_nonlinear);
+  class_read_double("halofit_min_k_max",ppr->halofit_min_k_max);
   class_read_double("halofit_k_per_decade",ppr->halofit_k_per_decade);
   class_read_double("halofit_sigma_precision",ppr->halofit_sigma_precision);
   class_read_double("halofit_tol_sigma",ppr->halofit_tol_sigma);
@@ -3379,19 +3379,11 @@ int input_default_precision ( struct precision * ppr ) {
    * - parameters related to nonlinear module
    */
 
-  ppr->halofit_dz=0.1;
-  ppr->halofit_min_k_nonlinear=0.0035;
+  ppr->halofit_min_k_nonlinear = 1.e-4;
+  ppr->halofit_min_k_max = 5.;
   ppr->halofit_k_per_decade = 80.;
-  ppr->halofit_sigma_precision=0.05; /**< a smaller value will lead to a
-                                        more precise halofit result at the *highest*
-                                        redshift at which halofit can make computations,
-                                        at the expense of requiring a larger k_max; but
-                                        this parameter is not relevant for the
-                                        precision on P_nl(k,z) at other redshifts, so
-                                        there is normally no need to change it */
-
-  ppr->halofit_min_k_max=5.;   // this one is actually used in the perturbation module
-  ppr->halofit_tol_sigma = 0.001;
+  ppr->halofit_sigma_precision = 0.05;
+  ppr->halofit_tol_sigma = 1.e-6;
 
   /**
    * - parameter related to lensing
