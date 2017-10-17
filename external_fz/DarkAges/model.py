Fixing normalization of spectra in the scenarios "PBH-evaporation" and "PBH-accretion"

external_fz/DarkAges/model.py
@@ -342,7 +342,7 @@ class evaporating_model(model):
 		spec_all = PBH_spectrum_at_m( mass_at_z[-1,:], logEnergies, 'ALL')
 		del_E = np.zeros(redshift.shape, dtype=np.float64)
 		for idx in xrange(del_E.shape[0]):
-			del_E[idx] = trapz(spec_all[:,idx]*E**2,(logEnergies))
+			del_E[idx] = trapz(spec_all[:,idx]*E**2*np.log(10),(logEnergies))
 			normalization = del_E
 
 		model.__init__(self, spec_el, spec_ph, normalization, 0)
@@ -398,6 +398,6 @@ class accreting_model(model):
 
 		spec_electrons = np.vectorize(_unscaled).__call__(redshift[None,:], spec_el[:,None])
 		spec_photons = np.vectorize(_unscaled).__call__(redshift[None,:], spec_ph[:,None])
-		normalization = trapz((spec_ph+spec_el)*E*np.log(10),logEnergies)*np.ones_like(redshift)
+		normalization = trapz((spec_ph+spec_el)*E**2*np.log(10),logEnergies)*np.ones_like(redshift)
 		# print normalization, spec_photons
 		model.__init__(self, spec_electrons, spec_photons, normalization, 0)

external_fz/DarkAges/special_functions.py
@@ -87,10 +87,10 @@ def luminosity_accreting_bh(Energy,recipe,PBH_mass):
 		a = -2.5+np.log10(Energy)/3.
 		Emin = (10/PBH_mass)**0.5
 		Ts = 200
-		if Energy.any() > Emin:
-			out = Energy**(-a)*np.exp(-Energy/Ts)
-		else:
-			out = 0.
+		out = np.zeros_like(Energy)
+		Emin_mask = Energy > Emin
+		out[Emin_mask] = Energy[Emin_mask]**(-a[Emin_mask])*np.exp(-Energy[Emin_mask]/Ts)
+		out[~Emin_mask] = 0.
 	else:
 		from .__init__ import DarkAgesError as err
 		raise err('I cannot understand the recipe "{0}"'.format(recipe))