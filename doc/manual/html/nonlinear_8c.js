55a99c61dfa2cb499ec406f54e89cc2e1c70c5c9
lesgourg <lesgourg@physik.rwth-aachen.de>
2018-09-10 18:02:26 +0200

    updated doc

--- doc/manual/html/nonlinear_8c.js
+++ doc/manual/html/nonlinear_8c.js
@@ -1,5 +1,5 @@
 var nonlinear_8c =
 [
     [ "nonlinear_init", "nonlinear_8c.html#a86727ddb48af0066973966308bb889cd", null ],
-    [ "nonlinear_halofit", "nonlinear_8c.html#a219d1d3acda1332d72fd9ca4a430e994", null ]
+    [ "nonlinear_halofit", "nonlinear_8c.html#a27ed37e9eeccad4ef1ba2c1bc8a42a64", null ]
 ];
