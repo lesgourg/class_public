cad6081548189e03905e74f3b7de25a28dc01387
lesgourg <lesgourg@physik.rwth-aachen.de>
2019-04-11 17:17:59 +0200

    updated version number to 2.7.2

--- include/common.h
+++ include/common.h
@@ -15,7 +15,7 @@
 #ifndef __COMMON__
 #define __COMMON__
 
-#define _VERSION_ "v2.7.1"
+#define _VERSION_ "v2.7.2"
 /* @cond INCLUDE_WITH_DOXYGEN */
 
 #define _TRUE_ 1 /**< integer associated to true statement */
