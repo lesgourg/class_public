48f790cc4a011129b8595448d741adfec286e1d5
lesgourg <lesgourg@physik.rwth-aachen.de>
2020-01-21 11:15:57 +0100

    removed 2.8.2

--- class_public-2.8.2/doc/manual/html/nonlinear_8h_structnonlinear__workspace.js
+++
@@ -1,6 +0,0 @@
-var nonlinear_8h_structnonlinear__workspace =
-[
-    [ "stab", "nonlinear_8h.html#a10d478d6232f4b80e685a27e74fd345b", null ],
-    [ "ddstab", "nonlinear_8h.html#af862225245ee4c635223517f6f275b81", null ],
-    [ "growtable", "nonlinear_8h.html#abca1425adb4b73f2b8a21a4d889e1600", null ]
-];
--- class_public-2.8.2/doc/manual/html/search/classes_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['nonlinear',['nonlinear',['../nonlinear_8h.html#structnonlinear',1,'']]],
-  ['nonlinear_5fworkspace',['nonlinear_workspace',['../nonlinear_8h.html#structnonlinear__workspace',1,'']]]
-];
