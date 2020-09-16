ac2288b13eab808318aa7755525fc924d04f94f3
Georgios Samaras <georgios.samaras@rwth-aachen.de>
2020-09-16 11:35:04 +0200

    removed old unneeded code

--- python/nn/models/T0_reio.py
+++ python/nn/models/T0_reio.py
@@ -7,76 +7,10 @@ import torch.nn.functional as F
 
 import h5py as h5
 
-import pytorch_spline
-
 from .model import Model
 from . import common
 from .. import utils
 from .. import time_slicing
-
-class BasisDecompositionNet(nn.Module):
-    def __init__(self, k, n_inputs_cosmo, n_inputs_tau, n_k):
-        super().__init__()
-
-        n_approx = 1
-
-        self.k = k
-        self.logk = nn.Parameter(torch.log(self.k), requires_grad=False)
-
-        self.logk_spline = nn.Parameter(
-                torch.log(utils.powerspace(self.k[0], self.k[-1], 5, 40)).float(),
-                requires_grad=False
-                )
-
-        self.lin_cosmo_coeff = nn.Linear(n_inputs_cosmo, 20)
-        self.lin_tau_coeff = nn.Linear(n_inputs_tau, 200)
-
-        self.lin_coeff_1 = nn.Linear(20 + 200, 100)
-        self.lin_coeff_2 = nn.Linear(100, len(self.logk_spline))
-
-        self.relu = nn.LeakyReLU(0.3)
-
-    def forward(self, x):
-        cosmo = common.get_inputs_cosmo(x)
-        tau_g = common.get_inputs_tau_reio(x)
-
-        # approx_reio = x["t0_reio_approx_1"]
-
-        y_cosmo = self.relu(self.lin_cosmo_coeff(cosmo))
-        y_tau = self.relu(self.lin_tau_coeff(tau_g))
-
-        y_spline = self.relu(self.lin_coeff_1(torch.cat((y_cosmo, y_tau), 1)))
-        y_spline = self.lin_coeff_2(y_spline)
-
-        return pytorch_spline.CubicSpline(self.logk_spline, y_spline)(self.logk)
-
-class CorrectionNet(nn.Module):
-
-    def __init__(self, n_inputs_cosmo, n_inputs_tau, n_k):
-        super().__init__()
-
-        self.lin_cosmo_corr = nn.Linear(n_inputs_cosmo, 200)
-        self.lin_tau_corr = nn.Linear(n_inputs_tau, 200)
-
-        self.lin_corr_1 = nn.Linear(200 + 200, 100)
-        self.lin_corr_2 = nn.Linear(100, 200)
-        self.lin_corr_3 = nn.Linear(200, n_k)
-
-        self.relu = nn.LeakyReLU(0.3)
-
-    def forward(self, x):
-        cosmo = common.get_inputs_cosmo(x)
-        tau_g = common.get_inputs_tau_reio(x)
-
-        y_cosmo = self.relu(self.lin_cosmo_corr(cosmo))
-        y_tau = self.relu(self.lin_tau_corr(tau_g))
-
-        y = torch.cat((y_cosmo, y_tau), axis=1)
-        y = self.relu(self.lin_corr_1(y))
-        y = self.relu(self.lin_corr_2(y))
-        y = self.lin_corr_3(y)
-
-        return y
 
 class Net_ST0_Reio(Model):
 
