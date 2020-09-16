adc2240d8827961ffc10daab57fdc6cbe76e8321
Georgios Samaras <georgios.samaras@rwth-aachen.de>
2020-09-16 12:48:16 +0200

    added default for spectra(param) plots

--- python/nn/plotter.py
+++ python/nn/plotter.py
@@ -11,8 +11,8 @@ class Plotter:
     def plot_training_histories(self):
         HistoryPlotter(self.workspace).plot_and_save()
 
-    def plot_spectra(self):
-        SpectraPlotter(self.workspace).plot()
+    def plot_spectra(self, include_params=False):
+        SpectraPlotter(self.workspace).plot(include_params=include_params)
 
     def plot_scatter_errors(self):
         TrianglePlotter(self.workspace).plot_and_save()
