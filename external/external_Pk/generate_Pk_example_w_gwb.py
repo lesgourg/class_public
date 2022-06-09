cbf6c56fd76c893ed627bae5fabd5a8c6548f1be
Florian Schulze <florian.tobias.schulze@rwth-aachen.de>
2022-06-09 12:22:14 +0200

    New external Pk for GWB

--- external/external_Pk/generate_Pk_example_w_gwb.py
+++ external/external_Pk/generate_Pk_example_w_gwb.py
@@ -1,68 +1,136 @@
 #!/usr/bin/python
-from __future__ import print_function
+
+# This is an example for a nice way to calculate all primordial spectra
+# and the background GW energy density in one python file.
 import sys
-from math import exp
+import numpy as np
+from scipy import constants
 
-# README:
-#
-# This is an example python script for the external_Pk mode of Class.
-# It generates the primordial spectrum of LambdaCDM.
-# It can be edited and used directly, though keeping a copy of it is recommended.
-#
-# Two (maybe three) things need to be edited:
-#
-# 1. The name of the parameters needed for the calculation of Pk.
-#    "sys.argv[1]" corresponds to "custom1" in Class, an so on
 
-try :
-    k_0           = float(sys.argv[1])
-    A_s           = float(sys.argv[2])
-    n_s           = float(sys.argv[3])
-    A_t           = float(sys.argv[4])
-    n_t           = float(sys.argv[5])
-    A_gwb         = float(sys.argv[6])
-    n_gwb         = float(sys.argv[7])
-    c_ad_gwb      = float(sys.argv[8])
+def read_parameter(argv=[]):
+    par = {}
+    # Standart values
+    par['k_min']   = 1e-6
+    par['k_max']   = 10
+    par['k_size']  = 1000
+    par['f_min']   = 0.5e-3
+    par['f_max']   = 2e2
+    par['f_size']  = 1000
+    
+    par['k_pivot'] = 0.05 # 1/Mpc
+    par['A_s']     = 2.215e-09
+    par['n_s']     = 0.9624
+    par['r']       = 0.07
+    par['n_t']     = -0.1
+    par['A_ini']   = 1e-10
+    par['n_ini']   = 0.
+    par['c_s_ini'] = 0.
 
-# Error control, no need to touch
-except IndexError :
-    raise IndexError("It seems you are calling this script with too few arguments.")
-except ValueError :
-    raise ValueError("It seems some of the arguments are not correctly formatted. "+
-                     "Remember that they must be floating point numbers.")
+    par['f_pivot'] = 1. # Hz
+    par['A_gw']    = 1e-5
+    par['n_gw']    = 0.
+    
+    # Read input parameter
+    try:
+        par['A_s']     = float(argv[0])
+        par['n_s']     = float(argv[1])
+        par['r']       = float(argv[2])
+        par['n_t']     = float(argv[3])
+        par['A_ini']   = float(argv[4])
+        par['n_ini']   = float(argv[5])
+        par['c_s_ini'] = float(argv[6])
 
-# 2. The function giving P(k), including the necessary import statements.
-#    Inside this function, you can use the parameters named in the previous step.
+        par['A_gw']    = float(argv[7])
+        par['n_gw']    = float(argv[8])
+    except IndexError:
+        pass
+    except ValueError:
+        raise ValueError("It seems some of the arguments are not correctly formatted. "+
+                         "Remember that they must be floating point numbers.")
+    
+    return par
 
-def P_s(k) :
-    return A_s * (k/k_0)**(n_s-1.)
+def init_ks(par):
+    """Return the ks array in 1/Mpc"""
+    return np.geomspace(par['k_min'], par['k_max'], par['k_size'])
 
-def P_t(k) :
-    return A_t * (k/k_0)**(n_t)
+def init_fs(par):
+    """Return the fs array in Hz"""
+    return np.geomspace(par['f_min'], par['f_max'], par['f_size'])
 
-def P_gwb(k) :
-    return A_gwb * (k/k_0)**(n_gwb)
 
-# 3. Limits for k and precision:
-#    Check that the boundaries are correct for your case.
-#    It is safer to set k_per_decade primordial slightly bigger than that of Class.
+def f_to_k(f):
+    """Convert frequency f to comoving wavenumber k"""
+    return f * (2 * np.pi) / constants.c * (1e6 * constants.parsec)  # 1/Mpc
 
-k_min  = 1.e-6
-k_max  = 10.
-k_per_decade_primordial = 200.
+def k_to_f(k):
+    """Convert comoving wavenumber k to frequency f"""
+    return k / (2 * np.pi) * constants.c / (1e6 * constants.parsec)  # Hz
 
-#
-# And nothing should need to be edited from here on.
-#
 
-# Filling the array of k's
-ks = [float(k_min)]
-while ks[-1] <= float(k_max) :
-    ks.append(ks[-1]*10.**(1./float(k_per_decade_primordial)))
+# Here we define the functions for the primordial spectra
+def P_s(k, par):
+    return par['A_s'] * (k/par['k_pivot'])**(par['n_s']-1.)
 
-# Filling the array of Pk's
-print("# Dimensionless primordial spectrum, equal to [k^3/2pi^2] P(k)")
-print("# k [1/Mpc]                P_scalar(k)                P_gwb(k)                   ad x gwb                   P_tensor(k)")
-for k in ks :
-    print("%.18e   %.18e   %.18e   %.18e   %.18e" % (k, P_s(k), P_gwb(k), c_ad_gwb, P_t(k)))
+def P_t(k, par):
+    return par['r']*par['A_s'] * (k/par['k_pivot'])**(par['n_t'])
 
+def P_gwb(k, par):
+    return par['A_ini'] * (k/par['k_pivot'])**(par['n_ini'])
+
+def cross_s_gwb(k, par):
+    return par['c_s_ini']
+
+# Background energy density of GWs
+def Omega_GW(f, par):
+    return par['A_gw'] * (f/par['f_pivot'])**(par['n_gw'])
+
+
+# Print functions
+def print_Pks(ks, par):
+    print("# Dimensionless primordial spectrum, equal to [k^3/2pi^2] P(k)")
+    print("# k [1/Mpc]                P_scalar(k)                P_gwb(k)                   ad x gwb                   P_tensor(k)")
+    for k in ks:
+        print("%.18e   %.18e   %.18e   %.18e   %.18e" % (k, P_s(k, par), P_gwb(k, par), cross_s_gwb(k, par), P_t(k, par)))
+
+def print_Omega_GW(fs, par):
+    print("# Dimensionless graviational wave background energy density Omega_GW(f)")
+    print("# f [Hz]                   Omega_GW(f)")
+    for f in fs:
+        print("%.18e   %.18e" % (f, Omega_GW(f, par)))
+
+
+def main(argv):
+    if len(argv) == 1:
+        raise Exception("Specify if you want to calculate the 'Pk' or 'OmGW'!")
+        return 1
+
+    if argv[1] == 'Pk':
+        # Calaculate the P(k) spectrum
+        # Initialize variables
+        argv = argv[2:]
+        par = read_parameter(argv)
+        ks = init_ks(par)
+
+        # Output spectrum
+        print_Pks(ks, par)
+    
+    elif argv[1] == 'OmGW':
+        # Calaculate the Omega_GW spectrum
+        # Initialize variables
+        argv = argv[2:]
+        par = read_parameter(argv)
+        fs = init_fs(par)
+
+        # Output spectrum
+        print_Omega_GW(fs, par)
+
+    else:
+        raise Exception("Can't interpret %s as input. You have to choose 'Pk' or 'OmGW'!" % argv[1])
+        return 1
+
+    return 0
+
+
+if __name__ == '__main__':
+    main(sys.argv)
