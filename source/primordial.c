1a4a031cabc1ec4500b4b7b8681ae0171ea4a4e1
s-ilic <stephane.ilic@gmail.com>
2022-11-16 19:48:32 +0100

    Corrected a likely typo

--- source/primordial.c
+++ source/primordial.c
@@ -802,7 +802,7 @@ int primordial_analytic_spectrum_init(
 
           if ((ppt->has_ad == _TRUE_) && (ppt->has_bi == _TRUE_) &&
               (((index_ic1 == ppt->index_ic_ad) && (index_ic2 == ppt->index_ic_bi)) ||
-               ((index_ic1 == ppt->index_ic_ad) && (index_ic1 == ppt->index_ic_bi)))) {
+               ((index_ic2 == ppt->index_ic_ad) && (index_ic1 == ppt->index_ic_bi)))) {
             one_correlation = ppm->c_ad_bi;
             one_tilt = ppm->n_ad_bi;
             one_running = ppm->alpha_ad_bi;
