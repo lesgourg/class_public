639b5e9c53942f8eb1acb8efafa9a8d6cc1fbdef
schoeneberg <32747859+schoeneberg@users.noreply.github.com>
2023-04-26 13:25:45 +0200

    Corrected uninitialized PhiL instead being set to zero (#126)
    * Corrected uninitialized PhiL instead being set to zero in the curved case
    
    * Whitespacing of the bug fix related to K>0
    
    ---------
    
    Co-authored-by: schoeneberg <schoeneberg@github.com>

--- tools/hyperspherical.c
+++ tools/hyperspherical.c
@@ -140,7 +140,6 @@ int hyperspherical_HIS_create(int K,
   firstprivate(lmax)
   {
     class_alloc_parallel(PhiL,(lmax+2)*sizeof(double)*_HYPER_CHUNK_,error_message);
-
     if ((K == 1) && ((int)(beta+0.2) == (lmax+1))) {
       /** Take care of special case lmax = beta-1.
           The routine below will try to compute
@@ -148,7 +147,10 @@ int hyperspherical_HIS_create(int K,
           the purpose is to calculate the derivative
           Phi'_{lmax}, and the formula is correct if we set Phi_{lmax+1} = 0.
       */
-      PhiL[lmax+1] = 0.0;
+      for(j=0; j<(lmax+2)*_HYPER_CHUNK_; j++){
+        PhiL[j] = 0.0;
+      }
+
       lmax--;
     }
 
