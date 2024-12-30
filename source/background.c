0e745a42f482b8e846bd5cc4fb003ee8895191d6
JesusSotoChavarria <a329651@uach.mx>
2024-12-30 17:47:35 -0600

    Update background.c

--- source/background.c
+++ source/background.c
@@ -500,9 +500,6 @@ if(pba->has_UG == _TRUE_){
     rho_m += pvecback[pba->index_bg_rho_b];
    
   
-  pvecback[pba->index_bg_delta] =delta;
-  pvecback[pba->index_bg_a_start]= a_start;
-  pvecback[pba->index_bg_Delta_rho_Lambda]=Delta_rho_Lambda;
        
   }
 /* END MODIFICATION ML */
@@ -1188,14 +1185,6 @@ int background_indices(
 
   /* - put here additional ingredients that you want to appear in the
      normal vector */
-
-  /*----------------------------------------------------------------------*/
-  /* BEGIN MODIFICATION ML*/
-  class_define_index(pba->index_bg_delta,pba->has_UG,index_bg,1);
-  class_define_index(pba->index_bg_a_start,pba->has_UG,index_bg,1);
-  class_define_index(pba->index_bg_Delta_rho_Lambda,pba->has_UG,index_bg,1);
-  /* END MODIFICATION M */
-  /*----------------------------------------------------------------------*/
   /*    */
   /*    */
 
