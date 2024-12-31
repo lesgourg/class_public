d99cfb95080e8155da1cd8aec22a2842bdf01388
JesusSotoChavarria <a329651@uach.mx>
2024-12-31 00:28:28 -0600

    Update input.c

--- source/input.c
+++ source/input.c
@@ -6170,13 +6170,14 @@ int input_default_params(struct background *pba,
   pop->write_distortions = _FALSE_;
 
 
-  /* BEGIN MODIFICATION ML */
-  
-  pba->delta=0.22;
-  pba->a_start=0.8;
-  pba->Delta_rho_Lambda= -0.0003;
+  /* BEGIN MODIFICATION UG */
+  if (pba->has_UG == 1){
+   if (pba->a_start == 0. || pba->delta == 0. || pba->Delta_rho_Lambda == 0. ){
+     pba->delta=0.22;
+     pba->a_start=0.8;
+     pba->Delta_rho_Lambda= -0.0003;}}
 
-  /* END MODIFICATION ML */
+  /* END MODIFICATION UG */
 
    
   /** 2) Verbosity */
