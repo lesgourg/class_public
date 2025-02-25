6797187b0a7b92d31f1e492564332d9f365c1f4f
Jesus Chavarria <a329651@uach.mx>
2025-02-25 01:03:25 -0600

    Update background.h

--- include/background.h
+++ include/background.h
@@ -79,8 +79,10 @@ struct background
   double count;  /*to print count number of results in the terminal */
   
   short has_UG;
+  short model;
   
   double count_terminal;
+
 
   /* END MODIFICATION UG */
 /*-----------------------------------------------------------------------------*/
